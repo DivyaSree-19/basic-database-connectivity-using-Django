195bc54ddc35426b5c9c93c780de810e5740f6de
DivyaSree-19 <161328138+DivyaSree-19@users.noreply.github.com>
2024-12-24 12:00:48 +0530

    Delete database_sql directory

--- database_sql/my_application/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class data(models.Model):
-    Name=models.CharField(max_length=20,default=' ')
-    Age=models.IntegerField(default=' ')
-    Address=models.CharField(max_length=20,default=' ')
-   
-    Mail=models.CharField(max_length=50,default=' ')
