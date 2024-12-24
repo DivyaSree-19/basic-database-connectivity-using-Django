628dfa5ef8ee5d918f1573b10393d8d19f08d65f
Divyasree-19 <manisenthil0987@gmail.com>
2024-12-24 12:49:33 +0530

    Added new folder with files

---
+++ database_sql/my_application/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class data(models.Model):
+    Name=models.CharField(max_length=20,default=' ')
+    Age=models.IntegerField(default=' ')
+    Address=models.CharField(max_length=20,default=' ')  
+    Mail=models.CharField(max_length=50,default=' ')
+    
+    def __str__(self):
+            return self.Name
